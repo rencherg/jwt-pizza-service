5f5b07729420a56c8dedfd4590511617c5c228c4
rencherga <rencher.grant@gmail.com>
2025-03-25 20:19:51 -0600

    Final fix

--- src/service.js
+++ src/service.js
@@ -64,7 +64,7 @@ app.use('*', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  logger.logError(`Error: ${err.message}`, { stack: err.stack });
+  logger.logError({'Uncaught Exception': `${err.message}`, 'stack': err.stack });
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
   next();
 });
